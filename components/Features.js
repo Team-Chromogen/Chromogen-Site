64aac03db75601de330b1a29d73f57c5ac1b4652
Nicholas Shay <shay.nicholas@gmail.com>
2020-10-17 17:01:38 -0700

    format team profiles section
    Co-authored-by: Ryan rtumel123@gmail.com

--- components/Features.js
+++ components/Features.js
@@ -5,6 +5,11 @@ export default function Features() {
   return (
     <section id={styles.features}>
       <h1>Features</h1>
+      <ul id={styles.featuresList}>
+        <li>The first feature</li>
+        <li>The second feature</li>
+        <li>The third feature</li>
+      </ul>
     </section>
   );
 };
--- components/Team.js
+++ components/Team.js
@@ -1,25 +1,69 @@
 import React from 'react';
 import styles from './Team.module.css'
+import { IconContext } from 'react-icons';
+import { AiOutlineGithub } from 'react-icons/Ai'
+import { GrLinkedin } from 'react-icons/Gr'
 
 export default function Team() {
   return (
     <section id={styles.team}>
       <h1>Meet The Team</h1>
+      
       <h3 id="teamHeader">Chromogen 1.0</h3>
-      <div className="teamProfiles">
-        <div id="individual">Connor Rose Delisle</div>
-        <div id="individual">Michelle Holland</div>
-        <div id="individual">Jim Chen</div>
-        <div id="individual">Andy Wang</div>
+      <div id={styles.teamProfiles}>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Connor Rose Delisle</h3>
+          <div id={styles.profileLinks}>
+            <a><AiOutlineGithub /></a>
+            <a><GrLinkedin /></a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Michelle Holland</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Andy Wang</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Jim Chen</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
       </div>
+      <hr id={styles.hr}></hr>
       <h3 id="teamHeader">Chromogen 2.0</h3>
-      <div className="teamProfiles">
-        <div id="individual">Nicholas Shay</div>
-        <div id="individual">Jinseon Shin</div>
-        <div id="individual">Amy Yee</div>
-        <div id="individual">Cameron Greer</div>
-        <div id="individual">Ryan Tumel</div>
+      <div id={styles.teamProfiles}>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Nicholas Shay</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Jinseon Shin</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Amy Yee</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Cameron Greer</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
+        <div id="individual">
+          <img id={styles.profileImage} />
+          <h3>Ryan Tumel</h3>
+          <div id={styles.profileLinks}><a>Github </a><a>LinkedIn</a></div>
+        </div>
       </div>
+
     </section>
   );
 };
