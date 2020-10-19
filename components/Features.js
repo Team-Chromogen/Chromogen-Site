410ff69c3369576ef76cdb03cd2f546f0b67fde2
Jinseon Shin <jinseonshin@gmail.com>
2020-10-19 19:26:16 -0400

    add CSS styling - navigation bar, creating grids for all components

--- components/Features.js
+++ components/Features.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import styles from './css-modules/Features.module.css';
+import styles from "./css-modules/Features.module.css";
 
 export default class Features extends Component {
   render() {
@@ -11,26 +11,26 @@ export default class Features extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
     };
     return (
       <section id={styles.features}>
-          <Slider {...settings}>
-            {/* <h1>Features</h1> */}
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-          </Slider>
+        <Slider {...settings}>
+          {/* <h1>Features</h1> */}
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+        </Slider>
       </section>
     );
   }
-};
+}
