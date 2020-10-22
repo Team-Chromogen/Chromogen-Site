0cadd8088b029d2a14f3c4be4d127c3ab679f17c
Nicholas Shay <shay.nicholas@gmail.com>
2020-10-22 09:41:08 -0700

    add favicon and head title to site
    Co-authored-by: Ryan rtumel123@gmail.com

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/style.css";
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +8,16 @@ export default function MyApp({ Component, pageProps }) {
         @import
         url('https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet');
       </style>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+        <title>Chromogen</title>
+      </Head>
       <Component {...pageProps} />
     </div>
   );
